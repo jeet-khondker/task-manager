c4af634201ea34b74ffd82a19cf7ce8b61883017
Jeet Khondker <jeet.usolutionsgroup@gmail.com>
2020-01-25 08:01:11 +0900

    Templating

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from flask import render_template
 from app import app
 
 # Home Page URL Route
 @app.route('/')
 @app.route("/index")
 def index():
-    return "Task Management System"
+    return render_template("index.html")
